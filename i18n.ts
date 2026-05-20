d8edcff845532ad91f69b383cab7cb349166fb89
uviolette <violette@uneek1.com>
2026-05-20 19:03:39 -0400

    Create i18n.ts

---
+++ i18n.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import enTranslations from './locales/en.json';
+import arTranslations from './locales/ar.json';
+import esTranslations from './locales/es.json';
+import zhTranslations from './locales/zh.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslations },
+      ar: { translation: arTranslations },
+      es: { translation: esTranslations },
+      zh: { translation: zhTranslations },
+    },
+    lng: localStorage.getItem('language') || 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
